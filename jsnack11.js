3712be073ddcc5d506b2997729e5641821bc916e
LoanaWitch <loanawitch@live.com>
2018-03-26 02:50:37 +0200

    completo con array di !=lunghezza

--- jsnack11.js
+++ jsnack11.js
@@ -1,12 +1,34 @@
 var letters = ["a", "b", "c", "d", "e"];
-var numbers = [1,2,3,4,5];
+var numbers = [1,2,3,4,5, 6, 7];
 var mixed = [];
 
-
-for (var i = 0; i < letters.length; i++) {
-    mixed.push(letters[i])
-    mixed.push(numbers[i])
+if (letters.length==numbers.length) {
+    for (var i = 0; i < letters.length; i++) {
+        mixed.push(letters[i])
+        mixed.push(numbers[i])
+    }
+}
+else if (letters.length>numbers.length) {
+  cont = 0
+  for (var i = 0; i < numbers.length; i++) {
+        mixed.push(letters[i])
+        mixed.push(numbers[i])
+        cont++
+    }
+    for (var k = cont; k < letters.length; k++) {
+      mixed.push(letters[k])
+    }
+}
+else { //sistema alternativo senza var cont
+  for (i = 0; i < letters.length; i++) {
+        mixed.push(letters[i])
+        mixed.push(numbers[i])
+            }
+    for (i; i < numbers.length; i++) {
+      mixed.push(numbers[i])
+    }
 }
 
+
 document.write (mixed)
-console.log (mixed);
+console.log(mixed);
