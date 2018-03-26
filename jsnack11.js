660bc2c237b016f0544c58527862f8a68f4fde28
LoanaWitch <loanawitch@live.com>
2018-03-26 02:54:08 +0200

    js11 rifinito

--- jsnack11.js
+++ jsnack11.js
@@ -1,5 +1,5 @@
 var letters = ["a", "b", "c", "d", "e"];
-var numbers = [1,2,3,4,5, 6, 7];
+var numbers = [1,2,3,4,5,6,7];
 var mixed = [];
 
 if (letters.length==numbers.length) {
@@ -10,25 +10,24 @@ if (letters.length==numbers.length) {
 }
 else if (letters.length>numbers.length) {
   cont = 0
-  for (var i = 0; i < numbers.length; i++) {
-        mixed.push(letters[i])
-        mixed.push(numbers[i])
-        cont++
-    }
+    for (var i = 0; i < numbers.length; i++) {
+          mixed.push(letters[i])
+          mixed.push(numbers[i])
+          cont++
+      }
     for (var k = cont; k < letters.length; k++) {
       mixed.push(letters[k])
     }
 }
 else { //sistema alternativo senza var cont
-  for (i = 0; i < letters.length; i++) {
-        mixed.push(letters[i])
-        mixed.push(numbers[i])
-            }
+    for (i = 0; i < letters.length; i++) {
+          mixed.push(letters[i])
+          mixed.push(numbers[i])
+    }
     for (i; i < numbers.length; i++) {
       mixed.push(numbers[i])
     }
 }
 
 
-document.write (mixed)
 console.log(mixed);
